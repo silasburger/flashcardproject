43065ef60eb05ffc4926e03ab5ed4fe2ee5dfd7d
Zac B <zacbennett0@gmail.com>
2018-11-06 19:20:41 -0800

    added multiple flashcard to same page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Card from './Card'
+import CardList from './CardList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Card />
+        <CardList />
       </div>
     );
   }
--- src/Card.js
+++ src/Card.js
@@ -4,10 +4,21 @@ import './Card.css';
 class Card extends Component {
   render() {
     return (
-      <div class="flip-card">
-        <div class="flip-card-inner">
-          <div class="flip-card-front">What day is today?</div>
-          <div class="flip-card-back">Fuck off.</div>
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            {/* Question to Ask  */}
+            {this.props.question}
+          </div>
+          <div className="flip-card-back">
+            {/* Answer to Question */}
+            {this.props.answer}
+            <div className="Buttons">
+              <button>Again</button>
+              <button>Good</button>
+              <button>Easy</button>
+            </div>
+          </div>
         </div>
       </div>
     );
